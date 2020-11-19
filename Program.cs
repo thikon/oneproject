d68001dde3219e4893b0c329184ac74784b716b0
Thitikorn V <thitikorn@intinsights.com>
2020-11-19 11:42:28 +0700

    add files to one project

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using submodulestest;
+using System;
+
+namespace mainmodule
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var temp = new Class1("dew","test1");
+            Console.WriteLine(temp.GetFullname());
+        }
+    }
+}
